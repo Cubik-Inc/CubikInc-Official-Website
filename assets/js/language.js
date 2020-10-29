5b3f46a0b95019c4c35daea9d752446d5956eb66
KayshaTown <KayshaTown@users.noreply.github.com>
2020-10-28 23:05:52 -0400

    Multi Language Support

---
+++ assets/js/language.js
@@ -0,0 +1,41 @@
+var arrLang = {
+  "en": {
+    "INTRO": "Introduction",
+    "WORK": "Our Projects",
+    "CONTACT": "Contact Us",
+  },
+  "zh": {
+    "INTRO": "关于我们",
+    "WORK": "项目展示",
+    "CONTACT": "联络我们",
+  }
+};
+
+// The default language is English
+var lang = "en";
+// Check for localStorage support
+if('localStorage' in window){
+   var lang = localStorage.getItem('lang') || navigator.language.slice(0, 2);
+   if (!Object.keys(arrLang).includes(lang)) lang = 'en';
+}
+
+$(document).ready(function() {
+  $(".lang").each(function(index, element) {
+    $(this).text(arrLang[lang][$(this).attr("key")]);
+  });
+});
+
+// get/set the selected language
+$(".translate").click(function() {
+  var lang = $(this).attr("id");
+
+  // update localStorage key
+  if('localStorage' in window){
+    localStorage.setItem('lang', lang);
+    console.log( localStorage.getItem('lang') );
+  }
+
+  $(".lang").each(function(index, element) {
+    $(this).text(arrLang[lang][$(this).attr("key")]);
+  });
+});
