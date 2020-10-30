0602a9d8da956fd84fd6ba81dc897b1267ad3a4e
Cubik65536 <cubik65536@qxorq.onmicrosoft.com>
2020-10-30 16:26:10 -0400

    Update index.html i

--- assets/js/language.js
+++ assets/js/language.js
@@ -1,14 +1,20 @@
 var arrLang = {
   "en": {
-    "INTRO": "Introduction",
-    "WORK": "Our Projects",
-    "CONTACT": "Contact Us",
+    "INTRO": "About Me",
+    "INTROINFO-A": "An individual software developer💻. Now using Swift⚒, Java and Python as development languages. I start to learn programming at 2018. I'm currently a student in a senconday school at Montréal, Québec, Canada.",
+    "WORK": "My Projects",
+    "CONTACT": "Contact Me",
+    "CONTACTINFO": "If you have some questions on our projects or need support on it, please write me an email about it.",
     "COPY": "Powered by Cubik65536｜Template Design: ",
   },
   "zh": {
-    "INTRO": "关于我们",
+    "INTRO": "关于我",
+    "INTROINFO-A": "一个个人开发者💻，熟悉的语言有Swift⚒，Java和Python。2018年开始学习敲代码。目前是一名中学生，坐标：加拿大魁北克省蒙特利尔。",
+    "INTROINFO-B": "有时间的话会玩F1和Minecraft。目前是Minecraft服务器",
+    "INTROINFO-C": "的管理组成员，负责其官网的运维。",
     "WORK": "项目展示",
-    "CONTACT": "联络我们",
+    "CONTACT": "联络我",
+    "CONTACTINFO": "如果您有关于我们项目的问题或者需要获得支持，请向我发送电子邮件。",
     "COPY": "由Cubik65536强力驱动｜模版设计：",
   }
 };
